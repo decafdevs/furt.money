ea42d2902433c07b89bf6e60ea851df65fd258e6
Suryadeep Pal <suryadeep10@gmail.com>
2022-03-17 13:11:35 +0530

    feat: initial commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="h-full flex flex-col-reverse items-end grid content-end">
+    <FurtLogo />
+    <div class="w-full flex flex-col grid justify-items-start">
+      <img
+        class="mx-10 mb-12 w-48"
+        src="badges/appstore.svg"
+        alt=""
+        srcset=""
+      />
+      <img
+        class="mx-10 mb-12 w-48"
+        src="badges/playstore.svg"
+        alt=""
+        srcset=""
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style>
+body {
+  background-color: #286D4D;
+  height: 100vh;
+}
+#__nuxt {
+  height: 100vh;
+}
+#__layout {
+  height: 100vh;
+}
+</style>
